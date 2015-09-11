f56606b9cdce30ec4b7212a97765766f2bb5e45a
Brandon Salmon <brandon.j.salmon@gmail.com>
2015-09-11 09:50:20 -0500

    Switch from grunt to gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var copy = require('gulp-copy');
+var uglify = require('gulp-uglify');
+
+gulp.task('compile:js', function () {
+	return gulp.src([
+		'bower_components/jquery/dist/jquery.js',
+		'bower_components/angular/angular.js',
+		'bower_components/highcharts/highcharts.js',
+		'bower_components/underscore/underscore.js',
+		'public/js/src/angular/app/app.js',
+		'public/js/src/angular/*.js',
+		'public/js/src/highcharts/**/*.js',
+		'public/js/src/*.js'
+	])
+		.pipe(uglify())
+		.pipe(concat('app.min.js'))
+		.pipe(gulp.dest('public/js'));
+});
+
+var watcher = gulp.watch(['bower_components/**/*.js', 'public/js/src/**/*.js'], ['compile:js']);
+watcher.on('change', function (event) {
+	console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+});
